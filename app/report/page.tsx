2432ea245a846a575bf5a41f7ce2b1d01a646865
Jack <ferreira.marlon@live.com>
2026-08-06 00:40:43 -0300

    feat: addition of displayed product selection and sales details to the report

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,6 +1,7 @@
 import { Badge } from "@/_components/ui/badge";
 import { Card, CardContent } from "@/_components/ui/card";
 import { Header } from "@/_components/ui/header";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/_components/ui/select";
 import { Separator } from "@/_components/ui/separator";
 import { ListChecks, LucideIcon, SquarePen, TicketPercent, Wallet } from "lucide-react";
 
@@ -9,6 +10,26 @@ interface CardDetail {
   value: string;
   Icon: LucideIcon;
 }
+
+interface ReportDetail {
+  title: string;
+  value: string;
+}
+
+const reportDetails: ReportDetail[] = [
+  {
+    title: "Quantidade",
+    value: "10",
+  },
+  {
+    title: "Receita",
+    value: "R$ 10",
+  },
+  {
+    title: "Pedidos",
+    value: "10",
+  },
+];
 
 const cardDetails: CardDetail[] = [
   {
@@ -55,6 +76,39 @@ export default function ReportPage() {
               </CardContent>
             </Card>
           ))}
+        </div>
+
+        <div className="p-4 flex gap-4 w-full">
+          <Card className="flex-1">
+            <CardContent className="flex flex-col gap-4">
+              <div className="flex justify-between items-center gap-4">
+                <div>
+                  <p className="font-medium">Consulta por produto</p>
+                  <p className="text-xs text-muted-foreground">Vendas de um item específico em semana.</p>
+                </div>
+                <div>
+                  <Select defaultValue="produto1">
+                    <SelectTrigger className="w-45 shadow">
+                      <SelectValue placeholder="Selecione um produto" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="produto1">Espresso</SelectItem>
+                      <SelectItem value="produto2">Latte</SelectItem>
+                      <SelectItem value="produto3">Mocha</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+              <div className="flex flex-row items-center gap-4">
+                {reportDetails.map((reportDetail) => (
+                  <Badge key={reportDetail.title} variant="outline" className="p-6 flex flex-col rounded-sm">
+                    <p>{reportDetail.title}</p>
+                    <span>{reportDetail.value}</span>
+                  </Badge>
+                ))}
+              </div>
+            </CardContent>
+          </Card>
         </div>
 
         <div className="p-4 flex gap-4 w-full">
