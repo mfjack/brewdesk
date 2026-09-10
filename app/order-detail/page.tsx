4791b107c4543581cc378e9fff073abdf07c36fe
Jack <ferreira.marlon@live.com>
2026-09-09 21:58:57 -0300

    feat: remove mt

--- app/order-detail/page.tsx
+++ app/order-detail/page.tsx
@@ -112,7 +112,7 @@ export default function OrderDetailPage() {
                   </p>
                 )}
 
-                <Button asChild className="w-full mt-4" size="lg" variant="default">
+                <Button asChild className="w-full mt-2" size="lg" variant="default">
                   <Link href={`/order?orderId=${order.id}`}>Detalhes da comanda</Link>
                 </Button>
 
